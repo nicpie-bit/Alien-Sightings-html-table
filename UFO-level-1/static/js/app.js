8853dd7255853556f6158e88f31548ef080614ac
nmbrouss <nicolebroussard@Nicoles-MacBook-Air.local>
2021-06-03 19:16:11 -0400

    referenced the tbody and used forEach to sort and display ufo data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,26 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get reference to table body
+var tbody = d3.select("#ufo-table>tbody")
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#form");
+
+// BONUS: Refactor to use Arrow Functions!
+tableData.forEach((ufoSiting) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoSiting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+});
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+
