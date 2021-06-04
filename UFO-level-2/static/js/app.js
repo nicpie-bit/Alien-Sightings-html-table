1676de45365e8024fdba878327e3bf1b5064248f
nmbrouss <nicolebroussard@Nicoles-MacBook-Air.local>
2021-06-03 20:52:04 -0400

    Copied over the stuff from level-1 to build upon for level 2

---
+++ UFO-level-2/static/js/app.js
@@ -0,0 +1,55 @@
+// from data.js
+var tableData = data;
+
+// Get reference to table body
+var tbody = d3.select("#ufo-table>tbody")
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#form");
+
+// Loop through data.js and use d3 to append and update each cell
+function buildTable(event) {
+    //Clear existing table
+    tbody.html("");
+    event.forEach((ufoSiting) => {
+        var row = tbody.append("tr");
+        Object.entries(ufoSiting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+        });
+    });
+};
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form 
+function runEnter() {
+    // Prevent page from refreshing
+    event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    // Show what was inputted
+    console.log(inputValue);
+    
+    //filter through data based on input
+    var filteredData = tableData.filter(entry => entry.datetime === inputValue);
+    
+    // Show filtered data
+    console.log(filteredData);
+
+    //Put filteredData into build function
+    buildTable(filteredData);
+};
+
+// Build table using data.js
+buildTable(tableData);
