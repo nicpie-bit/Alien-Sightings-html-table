98a1bdc2c1e719d2e0e48a778e869d0b427d5a87
nmbrouss <nicolebroussard@Nicoles-MacBook-Air.local>
2021-06-03 20:17:05 -0400

    Finsihed filtering function

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -11,14 +11,15 @@ var button = d3.select("#filter-btn");
 var form = d3.select("#form");
 
 // Loop through data.js and use d3 to append and update each cell
-tableData.forEach((ufoSiting) => {
-    var row = tbody.append("tr");
-    Object.entries(ufoSiting).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
+function buildTable(data) {
+    tableData.forEach((ufoSiting) => {
+        var row = tbody.append("tr");
+        Object.entries(ufoSiting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+        });
     });
-});
-
+};
 // Create event handlers 
 button.on("click", runEnter);
 form.on("submit",runEnter);
@@ -34,5 +35,16 @@ function runEnter() {
     // Get the value property of the input element
     var inputValue = inputElement.property("value");
 
+    // Show what was inputted
+    console.log(inputValue);
     
-}
+    //filter through data based on input
+    var filteredData = tableData.filter(entry => entry.datetime === inputValue);
+    
+    // Show filtered data
+    console.log(filteredData);
+
+    //Set tbody to the inputValue
+    
+
+};
